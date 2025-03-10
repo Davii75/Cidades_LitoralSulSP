bf0e8e079f8499bdc0e992ece3776b1f1ca8e23a
Davi de Castro Oliveira <davicastrooliveira@outlook.com>
2025-03-10 09:46:28 -0300

    Finaliza programa que executa o nome das cidades do Litoral Sul paulista

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Itanhaém");
+Console.WriteLine("Peruíbe");
+Console.WriteLine("Cananéia");
+Console.WriteLine("Iguape");
+Console.WriteLine("Ilha Comprida");
+Console.WriteLine("Mongaguá");
+Console.WriteLine("Registro");
